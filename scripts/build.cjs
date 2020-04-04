aaff736b010d193ab56d8756579c7acce79753cd
James Forbes <james.a.forbes@gmail.com>
2020-04-04 17:16:48 +1100

    Create minified browser build.

---
+++ scripts/build.cjs
@@ -0,0 +1,55 @@
+const { rollup } = require('rollup')
+const common = require('@rollup/plugin-commonjs')
+const resolve = require('@rollup/plugin-node-resolve')
+const { terser } = require('rollup-plugin-terser')
+const path = require('path')
+const fs = require('fs-extra')
+
+const input = path.resolve(__dirname, '../index.js')
+const normal = path.resolve(__dirname, '../dist/vella.js')
+const minified = path.resolve(__dirname, '../dist/vella.min.js')
+const dist = path.dirname(minified)
+
+const clear = fs.remove(dist)
+
+const bundle = 
+	clear.then(
+		() => rollup({
+			input,
+			plugins: [
+					common(),
+					resolve()
+			]
+		})
+	)
+
+const normalOutput =
+	bundle.then( 
+		x => x.generate({
+			name: 'V'
+		})
+		.then( x => 
+			fs.ensureDir(dist).then(
+				fs.writeFile(normal, x.output[0].code)
+			)
+		)
+	)
+
+const minifiedOutput =
+	bundle.then( 
+		x => x.generate({
+			plugins: [
+					terser()
+			],
+			sourcemap: true,
+			name: 'V'
+		})
+		.then( x => 
+			fs.ensureDir(dist).then(
+				fs.writeFile(minified, x.output[0].code)
+			)
+		)
+	)
+
+Promise.all([minifiedOutput, normalOutput])
+	.catch(console.error)
