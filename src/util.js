cd12b70442d77f3c79995c9276b15b4d4423079f
Pierre-Yves Gérardy <py@py.gy>
2020-04-12 01:13:18 +0200

    Add eslint as a npm script

--- src/util.js
+++ src/util.js
@@ -4,6 +4,6 @@ export function setDocument(d) {doc = d}
 // gets down to the value of nested streams.
 // in other words `flatten`, but where's the fun with that?
 export function absorb(f) {
-    do {f = f()} while (typeof f === "function")
-    return f
+	do {f = f()} while (typeof f === "function")
+	return f
 }
